510e8cd6eabd47c02a1e9e7577c0932987451023
Erfan Kheyrollahi <ekm507@gmail.com>
2024-02-06 02:41:48 +0330

    check whether if note has ever been updated in show_note_detailed function

--- makenote/dbmanager.py
+++ makenote/dbmanager.py
@@ -137,6 +137,7 @@ def show_note_detailed(books_directory:str, book_filename:str, note_id:int):
     metadata = json.loads(record[4].decode("utf-8"))
 
     category = f" \u001b[35m⭐{r[3]} " if record[3] != 0 else ""
+    updated = " \u001b[33mU" if "last_updated" in metadata.keys() else ""
 
 def get_note(books_directory, book_filename, note_id: int):
     try:
